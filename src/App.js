a8424d59fcf3f2c082247cf799c043251636b82e
Bianca <umemurabianca@gmail.com>
2022-01-17 14:15:24 -0300

    Componente ContadorDeCliques

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ContadorDeCliques from './ContadorDeCliques';
+import React,{useState} from 'react'
 function App() {
+  const [quantidade,setQuantidade]=useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContadorDeCliques
+        textoDescritivo="Quantidade de cliques acima"
+        textoBotao="Botão superior"
+        setQuantidade={setQuantidade}
+        quantidade={quantidade}
+      />
+      <ContadorDeCliques
+        textoDescritivo="Quantidade de cliques abaixo"
+        textoBotao="Botão inferiror"
+        setQuantidade={setQuantidade}
+        quantidade={quantidade}
+      />
     </div>
   );
 }
